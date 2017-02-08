3ee2aa715850068222abf26071459b62c92e9fd8
Austin Walker <bgbrwr@gmail.com>
2017-02-07 18:25:40 -0700

    change swal location

--- public/js/controllers/emailCtrl.js
+++ public/js/controllers/emailCtrl.js
@@ -2,6 +2,5 @@ angular.module('jazz').controller('emailCtrl', function($scope, emailService) {
 
     $scope.sendEmail = function(user) {
         emailService.email(user);
-        swal('Email sent!', 'We will contact you soon!', 'success')
     };
 });
--- public/js/services/emailService.js
+++ public/js/services/emailService.js
@@ -2,6 +2,9 @@ angular.module("jazz").service("emailService", function($http) {
     this.email = function(user) {
         return $http.post("/contact", user).then(function(httpResponse) {
             console.log('response:', httpResponse);
+            if (httpResponse.status === 200) {
+              return swal('Email sent!', 'We will contact you soon!', 'success');
+            }
         });
     }
 });
